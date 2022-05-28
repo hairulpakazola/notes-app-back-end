815cd3c5359aa6879f3480eff39a0472143daec0
Hairul <hairulpakazola@gmail.com>
2022-05-29 06:29:04 +0900

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,46 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require("./handler");
+
+const routes = [
+  // routes Menyimpan data notes
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNoteHandler,
+  },
+
+  // routes Menampilkan data notes
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+  },
+
+  // routes Menampilkan isi data notes
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+
+  // routes Mengedit isi data notes
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
+
+  // routes Menghapus data notes
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
